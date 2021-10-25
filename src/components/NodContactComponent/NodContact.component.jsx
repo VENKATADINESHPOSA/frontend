354e58cfa084358f8577a16341d484103077e1d0
Posa Venkata Dinesh <posavenkatadinesh@gmail.com>
2021-10-25 22:37:23 +0530

    added contact us page and styled it.

--- src/components/NodContactComponent/NodContact.component.jsx
+++ src/components/NodContactComponent/NodContact.component.jsx
@@ -1,11 +1,96 @@
-import { Typography } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Grid,
+  TextField,
+  Typography,
+  Button,
+} from "@material-ui/core";
 import React from "react";
 import "./NodContact.styles.scss";
 
 const NodContactComponent = () => {
   return (
-    <div style={{}}>
-      <Typography gutterBottom variant="h3" align="center">Contact Us</Typography>
+    <div style={{ marginTop: "180px" }}>
+      <Typography
+        gutterBottom
+        variant="h4"
+        align="center"
+        style={{ paddingTop: "20px", color: "#0c629f", fontWeight: "bold" }}
+      >
+        Contact Us
+      </Typography>
+      <Card
+        style={{ maxWidth: "500px", margin: "20px auto", padding: "20px 5px" }}
+      >
+        <CardContent>
+          <form>
+            <Grid container spacing={1}>
+              <Grid xs={12} sm={6} item>
+                <TextField
+                  label="First Name"
+                  placeholder="Enter your first name"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  classes={{ borderBottom: "0px" }}
+                />
+              </Grid>
+              <Grid xs={12} sm={6} item>
+                <TextField
+                  label="Last Name"
+                  placeholder="Enter your Last Name"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <TextField
+                  label="Email"
+                  type="email"
+                  placeholder="Enter your email"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <TextField
+                  label="Phone Number"
+                  type="number"
+                  placeholder="Enter your phone number"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  disableUnderline={true}
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <TextField
+                  label="Message"
+                  placeholder="Type your message here."
+                  variant="outlined"
+                  fullWidth
+                  required
+                  multiline
+                  rows={4}
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                >
+                  Submit
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 };
--- src/views/NodContactUsPage/NodContactUsPage.component.jsx
+++ src/views/NodContactUsPage/NodContactUsPage.component.jsx
@@ -2,25 +2,26 @@ import React from "react";
 import { connect } from "react-redux";
 import "./NodContactUsPage.styles.scss";
 import Header from "~/components/Header";
+import NodContactComponent from "../../components/NodContactComponent/NodContact.component";
 import Copyright from "../../components/CopyrightComponent/Copyright.component";
 import FooterNod from "../../components/FooterNodComponent/FooterNod.component";
 
-
 const NodContactUs = (props) => {
-    return (
-      <div className="nod-contactus-div">
-        <Header {...props} />
-        <FooterNod />
-        <Copyright />
-      </div>
-    );
-  };
-  
-  export default connect((state) => ({
-    ...state.user,
-    ...state.cartDetail,
-    ...state.updateProductData,
-    ...state.cartItemVal,
-    ...state.updateCartItemData,
-    ...state.removeCartData,
-  }))(NodContactUs);
+  return (
+    <div className="nod-contactus-div">
+      <Header {...props} />
+      <NodContactComponent />
+      <FooterNod />
+      <Copyright />
+    </div>
+  );
+};
+
+export default connect((state) => ({
+  ...state.user,
+  ...state.cartDetail,
+  ...state.updateProductData,
+  ...state.cartItemVal,
+  ...state.updateCartItemData,
+  ...state.removeCartData,
+}))(NodContactUs);
